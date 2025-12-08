484eaaa9a72c8dc14e7930177531a7884c69ca0e
gregory-gif <gregory@toileblanche.com>
2025-12-08 13:48:54 +0100

    Update __init__.py

--- ui/__init__.py
+++ ui/__init__.py
@@ -1,1 +1,5 @@
-
+from .dashboard import show_dashboard
+from .scorecard import Scorecard
+from .simulator import show_simulator
+from .session_log import show_session_log
+from .layout import create_layout
