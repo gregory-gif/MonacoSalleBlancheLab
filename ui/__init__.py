026bcd41862900f3514588d9a64e483c31cceaa5
gregory-gif <gregory@toileblanche.com>
2025-12-18 10:38:50 +0100

    Update __init__.py

--- ui/__init__.py
+++ ui/__init__.py
@@ -1,4 +1,5 @@
 from .dashboard import show_dashboard
 from .scorecard import show_scorecard  # Changed from Scorecard to show_scorecard
 from .simulator import show_simulator
+from .career_mode import show_career_mode
 from .session_log import show_session_log
